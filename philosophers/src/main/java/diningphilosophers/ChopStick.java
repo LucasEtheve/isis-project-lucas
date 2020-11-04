23b8b42277ef4711d895bedafb0ddd0498a044c9
LucasEtheve <57803978+LucasEtheve@users.noreply.github.com>
2020-11-04 12:31:49 +0100

    Philosophers
    Philosophers

---
+++ philosophers/src/main/java/diningphilosophers/ChopStick.java
@@ -0,0 +1,34 @@
+package diningphilosophers;
+
+public class ChopStick {
+    // Le nombre total de baguettes
+    private static int stickCount = 0;
+    // Le numéro de chaque baguette
+    private final int myNumber;
+    // Est-ce que ma baguette est libre ?
+    private boolean iAmFree = true;
+
+    public ChopStick() {
+        // Chaque baguette est numérotée 
+        myNumber = ++stickCount;
+    }
+
+    synchronized public void take() throws InterruptedException {
+        while (!iAmFree) {
+            wait();
+        }
+        iAmFree = false;
+        System.out.println("Stick " + myNumber + " pris");
+    }
+
+    synchronized public void release() {
+        System.out.println("Stick " + myNumber + " relache");
+        iAmFree = true;
+        notifyAll();
+    }
+    
+    @Override
+    public String toString() {
+        return "Stick#" + myNumber;
+    }
+}
